0f6321f1bba2060171cb678c62b498e63396d38c
Clement Motloutsi <clementtshepo3@gmail.com>
2023-06-23 16:01:28 +0200

    first commit

---
+++ src/Home/Data.js
@@ -0,0 +1,32 @@
+import img1 from '../../assets/img1.png'
+import img2 from '../../assets/img2.png'
+import img3 from '../../assets/img3.png'
+import img4 from '../../assets/img4.png'
+
+const serviceData = [
+    {
+        name:"Cleaning Services",
+        category:"Cleaner",
+        numberCompany:"60",
+        image:img1,
+        bgColor: "#D0386B"
+    },
+
+    {
+        name:"Plumbing Services",
+        category:"Plumber",
+        numberCompany:"20",
+        image:img2,
+        bgColor:"#9138A1"
+    },
+
+    {
+        name:"Hairdresser Services",
+        category:"Salon",
+        numberCompany:"10",
+        image:img3,
+        bgColor:"#3E9BC5"
+    }
+]
+
+export default serviceData;
